9c9460f89bd01d03be5d9b40244294c2cac69ae1
Jo Knightstep <jocoleknight@gmail.com>
2019-06-12 18:53:29 -0500

    JS flower

---
+++ blog/index.js
@@ -0,0 +1,10 @@
+Prompt(Blog JS)
+//this is to add a variable assigned to an element //
+
+let imageElement = document.getElementById("blogImage");    
+
+let alertFunction = function () {
+    alert("hello flower");
+}
+
+imageElement.addEventListener("click", alertFunction);
