54f6abecd6c6d6db528763e9fb089ddab93b520e
Jo Knightstep <jocoleknight@gmail.com>
2019-06-12 19:41:26 -0500

    array

---
+++ projects/index.js
@@ -0,0 +1,14 @@
+let seagull = "bird at sea";
+let seal = "ocean cat";
+let otter = "ocean cutie";
+let tiger = "big cat";
+let wolf = "hungry dog";
+
+console.log(seagull, seal, otter, tiger, wolf);
+
+//array of animals
+myArray = [];
+
+let animalTalk = 'I went to the zoo and saw a ' + seagull ;
+
+console.log(animalTalk)
