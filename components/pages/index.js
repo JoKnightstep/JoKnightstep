04dfc6103a34d37cc952096fa7d3386ba8500740
Jo Knightstep <jocoleknight@gmail.com>
2019-07-03 21:00:48 -0500

    broken nav

---
+++ components/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Blog } from './Blog';
+export { default as Conact} from './Contact';
+export { default as Home} from './Home';
+export { default as Portfolio } from './Portfolio;
